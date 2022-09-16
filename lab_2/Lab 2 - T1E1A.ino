2136fa53b7c55f8a45b2a3a3fb034ef8d8fe66b0
Justin Huang <justin9.ij@gmail.com>
2022-09-16 12:18:21 -0400

    Update Lab 2 - T1E1A.ino

--- lab_2/Lab 2 - T1E1A.ino
+++ lab_2/Lab 2 - T1E1A.ino
@@ -1,20 +1,13 @@
-#define TRIG_PIN 2
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode (TRIG_PIN, INPUT); //Sensor input
+  pinMode(2,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(TRIG_PIN) == 1)
-    {
-      Serial.println("Group 2 >> (Sensor reading is 1)");
-    }
-  else if (digitalRead(TRIG_PIN) == 0)
-    {
-      Serial.println("Group 2 >> (Sensor reading is 0)");
-    }
-  delayMicroseconds (10);
+  digitalWrite(2,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(2,LOW);
+  delay(50);
 }
