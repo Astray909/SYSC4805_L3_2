0881988bb36cd376b0f93708e9ef275396d11db6
Justin Huang <justin9.ij@gmail.com>
2022-09-16 12:10:40 -0400

    Update Lab 2 - T2E2.ino

--- lab_2/Lab 2 - T2E2.ino
+++ lab_2/Lab 2 - T2E2.ino
@@ -1,6 +1,3 @@
-double AREF = 3.3;
-double analogReader = analogRead(A0);
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -10,11 +7,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   delay(500);
-  Serial.print("L3, Group 2, ");
+  double AREF = 3.3;
+  double analogReader = analogRead(A0);
+  Serial.print("L3, Group 2, voltage = ");
   double voltage=(analogReader/1023)*AREF;
-  double distance_1 = pow (0.88, voltage);
-  double distance = 2.05*distance_1;
+  double distance_1 = pow (0.08, voltage);
+  double distance = 38*distance_1;
+  Serial.print(voltage);
+  Serial.print(", distance is ");
   Serial.print(distance);
+  Serial.print("cm");
   Serial.println("");
 
 }
