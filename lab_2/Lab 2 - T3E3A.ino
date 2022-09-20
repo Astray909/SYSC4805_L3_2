ccbed088b2564db6065d9ee9089039374286da46
Andy Ngo <48596160+andy-ngo@users.noreply.github.com>
2022-09-20 13:26:36 -0400

    Update Lab 2 - T3E3A.ino

--- lab_2/Lab 2 - T3E3A.ino
+++ lab_2/Lab 2 - T3E3A.ino
@@ -1,11 +1,12 @@
 #define TRIG_PIN 2
 #define ECHO_PIN 3
+unsigned long output;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(TRIG_PIN,OUTPUT);
-  pinMode(ECHO_PIN,OUTPUT);
+  pinMode(ECHO_PIN,INPUT);
 }
 
 void loop() {
@@ -14,5 +15,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN,LOW);
   pulseIn(ECHO_PIN, HIGH, 50000);
-  
+  Serial.print(output);
+  Serial.println(" Section 3 Group 2");
+  delay(60);
 }
