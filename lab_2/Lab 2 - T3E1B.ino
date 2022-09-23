6026099995a66c1423b51266f2542c659d11e504
Astray909 <justin9.ij@gmail.com>
2022-09-23 09:16:06 -0400

    jhuang feat

--- lab_2/Lab 2 - T3E1B.ino
+++ lab_2/Lab 2 - T3E1B.ino
@@ -1,9 +1,12 @@
+#define TRIG_PIN 2
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(TRIG_PIN,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  analogWrite(TRIG_PIN, 1);
 }
